3f7ff43416db2f23d3c5c25aadd4f80c0493b071
Siddhesh Deshmukh <s.s.deshmukh00@gmail.com>
2024-05-05 18:17:59 +0530

    update

--- application.py
+++ application.py
@@ -50,9 +50,9 @@ def fetch_issue():
         # Fetch the messages after the run is complete
         list_messages = openai.beta.threads.messages.list(thread_id=thread.id)
         
-        response_message = list_messages.data[1].content[-1].text.value
+        response_message = list_messages.data #[-1].content[-1].text.value
 
-        return jsonify({"issue_statement": response_message}), 200
+        return response_message #jsonify({"issue_statement": response_message}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
